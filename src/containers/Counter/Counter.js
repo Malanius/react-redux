d434d81651b9c22168f4d415a88cdc7f9d01fb1f
Malanius <malanius@seznam.cz>
2018-10-24 18:36:19 +0200

    Updating state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,7 +6,7 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
- 
+
     render() {
         return (
             <div>
@@ -15,13 +15,22 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)} />
                 <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store result</button>
+                <ul>
+                    {this.props.storedResults.map(strRes => (
+                        <li onClick={() => this.props.onDeleteResult(strRes.id)} key={strRes.id}>{strRes.value}</li>
+                    ))}
+
+                </ul>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state) => ({
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
 })
 
 const mapDispatchToProps = (dispatch) => {
@@ -29,7 +38,9 @@ const mapDispatchToProps = (dispatch) => {
         onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
         onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
         onAddCounter: (value) => dispatch({ type: 'ADD', value: value }),
-        onSubtractCounter: (value) => dispatch({ type: 'SUBTRACT', value: value })
+        onSubtractCounter: (value) => dispatch({ type: 'SUBTRACT', value: value }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultId: id })
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,26 +1,43 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1
+            return newState;
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
-
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: state.counter }) //concat returns new array, push doesn't
+            }
+        case 'DELETE_RESULT':
+            // const id = 2;
+            // const newResults = [...state.results]; //not a deep copy but ok for element removal
+            // newResults.splice(id, 1);
+            const updatedResults = state.results.filter(result => result.id !== action.resultId); //returns new array
+            return {
+                ...state,
+                results: updatedResults
             }
         default:
             return state;
