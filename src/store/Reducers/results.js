9f46ef43272881226c5e0aa3a18ff724c5468097
Malanius <malanius@seznam.cz>
2018-10-29 20:06:50 +0100

    Use utility function

--- src/store/Reducers/results.js
+++ src/store/Reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -9,19 +10,14 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             //Change data here?
-            return {
-                ...state,
-                results: state.results.concat({ id: + new Date(), value: action.result }) //concat returns new array, push doesn't
-            }
+            //concat returns new array, push doesn't
+            return updateObject(state, { results: state.results.concat({ id: + new Date(), value: action.result }) });
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newResults = [...state.results]; //not a deep copy but ok for element removal
             // newResults.splice(id, 1);
             const updatedResults = state.results.filter(result => result.id !== action.resultId); //returns new array
-            return {
-                ...state,
-                results: updatedResults
-            }
+            return updateObject(state, { results: updatedResults });
         default:
             return state;
     }
